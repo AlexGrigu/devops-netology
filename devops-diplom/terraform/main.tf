9fdba7837e403fbc2cebe3b0658e03700fecdbcf
Alexandr Grigorev <kobe3480@mail.ru>
2022-10-25 11:54:23 +0300

    Diplom v7_del_keys

--- devops-diplom/terraform/main.tf
+++ devops-diplom/terraform/main.tf
@@ -20,22 +20,9 @@ terraform {
     bucket = "alexgrigu.web"
     region = "ru-central1-a"
     key = "stage/terraform.tfstate"
-    access_key = "YCAJEsZKwr5nTxVpN0rAZYtwS"
-    secret_key = "YCMSQ42uozWVtERW2ZiKQwoS8AJyhgiCTiMYcdu8"
+    access_key = "access_key"
+    secret_key = "secret_key"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
 }
-
-#  metadata = {
-#    user-data = "${file("meta.txt")}"
-#  }
-#}
-#  metadata = {
-#    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
-#  }
-#}
-#resource yandex_compute_image ubu-img {
-#  name         = "ubuntu-20-04-lts-v20220620"
-#  source_image = "fd8f1tik9a7ap9ik2dg1"
-#}
